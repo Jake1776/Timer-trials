b173df997adebedd68a72b70d128ed34ac117811
Jake1776 <119820927+Jake1776@users.noreply.github.com>
2023-12-09 21:53:35 -0700

    update

--- trial14calculator/calculator-script.js
+++ trial14calculator/calculator-script.js
@@ -3,10 +3,13 @@ function calculateTotal() {
   var hoursWorked = parseFloat(document.getElementById('hoursWorked').value);
 
   if (isNaN(hourlyRate) || isNaN(hoursWorked)) {
-    alert('Please enter a valid numeric value');
+    alert('Please enter valid numeric values.');
     return;
   }
 
   var totalDollars = hourlyRate * hoursWorked;
   var timeInBnopx = totalDollars / hourlyRate;
+
+  document.getElementById('result').innerHTML = 'Total: $' + totalDollars.toFixed(2) + '<br>' +
+    'Equivalent time in Bnopx: ' + timeInBnopx.toFixed(2) + ' hours';
 }
